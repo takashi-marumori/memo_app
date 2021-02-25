9bf80d7a44f050eff241e2084638c5e039770f78
takashi-marumori <76422764+takashi-marumori@users.noreply.github.com>
2021-02-25 22:37:35 +0900

    Revert "CSS"
    This reverts commit 0b72649162cc763c20f6b77140b813c86edbfcc2.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({extended: false}));
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'secret',
+
   database: 'memo_app'
 });
 
